847f9b17b8991149f1a6fd1a8eb0803d70532a6a
kmoch <kevin.edholm.moch@gmail.com>
2019-11-13 16:30:08 -0800

    more soundboard more

--- src/soundboard.js
+++ src/soundboard.js
@@ -1,64 +1,61 @@
 export default class Soundboard {
   constructor() {
-    this.src = './assets/sounds/Dr_Mario_Theme.mp3';
-    this.isMuted = false;
+    this.themeSRC = './assets/sounds/Dr_Mario_Theme.mp3';
+    this.gameSRC = './assets/sounds/Fever_music.mp3';
+    this.src = this.themeSRC;
+    
+    this.createAudio();
+    // this.audio = document.createElement('audio');
+    // this.isMuted = false;
+
     this.muteButton = document.getElementById('mute-music');
-
-    this.gameOptions = document.getElementById('game-options');
+    // this.gameOptions = document.getElementById('game-options');
     this.startButton = document.getElementById('start-button');
-
     this.lostButton = document.getElementById('lost-game');
     this.wonButton = document.getElementById('won-game');
 
-    muteButton.addEventListener('click', () => {
-      this.isMuted = !this.isMuted;
+    this.muteButton.addEventListener('click', () => {
+      this.toggleMute();
+      // this.audio.muted = !this.audio.muted;
+      // this.isMuted = !this.isMuted;
       this.muteButton.classList.toggle('sound-off');
     });
 
-    // start music and associated event listeners
-    playMusic(this.src);
-
-    
+    this.startButton.addEventListener('click', () => {
+      this.audio.src = this.gameSRC;
+    });
 
     lostButton.addEventListener('click', () => {
-      playMusic('./assets/sounds/Dr_Mario_Theme.mp3');
+      this.audio.src = this.themeSRC;
     });
+
     wonButton.addEventListener('click', () => {
-      playMusic('./assets/sounds/Dr_Mario_Theme.mp3');
+      this.audio.src = this.themeSRC;
     });
-
+    
+    playMusic();
 
   }
 
-  playMusic (url) {
-    let audio = document.createElement('audio');
+  createAudio() {
+    this.audio = document.createElement('audio');
+    this.audio.src = this.src;
+    this.audio.style.display = "none";
+    this.audio.loop = "true";
+    // this.audio.onended = function () {
+    //   audio.remove();
+    // };
+    document.body.appendChild(this.audio);
+  }
 
-    if (musicIsMuted) {
-      audio.muted = true;
-    }
-
-    audio.src = url;
-    audio.style.display = "none";
-    audio.onended = function () {
-      audio.remove();
-    };
-    document.body.appendChild(audio);
-
-    const play = function () {
-      audio.play();
-    }
-    document.addEventListener('click', play);
-
-    let muteButton = document.getElementById('mute-music');
-    muteButton.addEventListener('click', () => {
-      audio.muted = !audio.muted;
-      // console.log('paused?');
-    });
-
-    let startButton = document.getElementById('start-button');
-    startButton.addEventListener('click', () => {
-      document.removeEventListener('click', play);
-      audio.remove();
+  playMusic () {
+    document.addEventListener('click', () => {
+      this.audio.play();
     });
   }
+
+  toggleMute() {
+    this.audio.muted = !this.audio.muted;
+  }
+
 }
